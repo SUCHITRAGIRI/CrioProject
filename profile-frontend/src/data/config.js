a571b33d485233a46ed949e095a019c10e6dc9de
SUCHITRA GIRI <suchigiri13@gmail.com>
2021-01-26 17:46:23 +0000

    Initial commit

--- profile-frontend/src/data/config.js
+++ profile-frontend/src/data/config.js
@@ -1,10 +1,10 @@
 
 module.exports = {
   // TODO: change your profile information here
-  name: "Ashok Kumar",
+  name: "Suchitra Giri",
   greeting: "Hey 👋",
-  greetingDescription: "I'm Ashok Kumar and I'm a Software Engineer!",
-  githubUrl: "https://github.com/ashokkumar95",
-  linkedinUrl: "https://linkedin.com/ashokkumar1",
+  greetingDescription: "I'm Suchitra Giri and I'm a Software Engineer!",
+  githubUrl: "https://github.com/SUCHITRAGIRI",
+  linkedinUrl: "https://www.linkedin.com/in/suchitra-giri/",
   cvLink: "https://docs.google.com/document/d/1E1234561NMyQA67890ygda7abcdefghijs1jBp7HFI0/",
 };
